e74ee88e5080bb4bc9f491aa91edbbb4c99da295
Alden Parker <ajparker1401@gmail.com>
2025-01-18 19:17:15 -0500

    Linted and added backtrace to console commands. Backtrace does not print correct debug info, but does not crash the machine anymore.

--- kern/hidden.h
+++ kern/hidden.h
@@ -1,6 +1,6 @@
 #include "inc/stdio.h"
-#include <inc/types.h>
-#include <inc/string.h>
 #include "inc/types.h"
+#include <inc/string.h>
+#include <inc/types.h>
 
-bool hidden_test_cases(); 
+bool hidden_test_cases();
